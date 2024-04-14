1ac295f3f84b91b13afdf5eddde0955de21d5034
hiki7 <rsaliyev03@gmail.com>
2024-04-15 03:04:31 +0500

    add: home component and add: styles for app and home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { TopicsComponent } from './topics/topics.component';
 import { QuestionsComponent } from './questions/questions.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: '/topics', pathMatch: 'full' },
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent},
     { path: 'topics', component: TopicsComponent },
     { path: 'topics/:topicId/questions', component: QuestionsComponent},
 ];
